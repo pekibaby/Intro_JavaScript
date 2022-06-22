84c7c8566c6c758938b91ffb22ac35e041a4aca8
Egonna Ngwumezie <103622341+pekibaby@users.noreply.github.com>
2022-06-22 13:15:57 +0100

    Create Intro.js

---
+++ Intro.js
@@ -0,0 +1,5 @@
+let name = "Egonna Cynthia Ngwumezie";
+let height = "5 ft 6";
+let country = "Nigeria";
+
+console.log(`My name is: ${name} \n My height is: ${height} \n My country is: ${country}`);
